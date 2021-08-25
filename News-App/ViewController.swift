49fc3d8eff3a5e2b0e69d2b952d0668a8ee47b47
nevzorov46 <masterpiece.nevzorov@gmail.com>
2021-08-25 14:39:47 +0300

    Initial Commit

---
+++ News-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News-App
+//
+//  Created by Иван Карамазов on 25.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
